558700b11ec175a178c45e9d462b49fd9f1de5cd
Joe Attardi <jattardi@constantcontact.com>
2016-06-14 13:09:05 -0400

    Move Main to components, add loading indicator

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Main from './Main';
-require('../css/main.css');
+import Main from './components/Main';
+import css from '../css/main.css';
 
 ReactDOM.render(<Main />, document.getElementById('app'));
 
---
+++ src/js/components/Main.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import {getAllPullRequests} from '../githubService';
+import PullRequest from './PullRequest';
+import LoadingIndicator from './LoadingIndicator';
+import config from '../../config/config.json';
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      pullRequests: []
+    };
+  }
+
+  componentWillMount() {
+    getAllPullRequests(config.repos).then(pullRequests => {
+      this.setState({
+        pullRequests,
+        loading: false
+      });
+    });
+  }
+
+  renderBody() {
+    if (this.state.loading) {
+      return <LoadingIndicator />;
+    } else {
+      return (
+        <div>
+          {this.state.pullRequests.map(pullRequest => {
+            return <PullRequest key={pullRequest.id} pullRequest={pullRequest} />;
+          })}
+        </div>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Pull Requests</h1>
+        {this.renderBody()}
+      </div>
+    );
+  }
+}
+
+export default Main;
