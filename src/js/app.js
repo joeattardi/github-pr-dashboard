1b6bfe5daaddef26d3b0c507d91ffe88056730dd
Joe Attardi <jattardi@constantcontact.com>
2016-06-14 11:30:01 -0400

    add CSS

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Main from './Main';
-
+require('../css/main.css');
 
 ReactDOM.render(<Main />, document.getElementById('app'));
 
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,10 @@ module.exports = {
   module: {
     loaders: [
       {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
         test: /\.json$/,
         loader: 'json-loader'
       },
