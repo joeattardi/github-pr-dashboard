6713d39dfb70163272524eba786761020e5fafc0
Joe Attardi <jattardi@constantcontact.com>
2016-06-15 11:26:09 -0400

    Add link to user profile

--- src/js/components/UserPhoto.jsx
+++ src/js/components/UserPhoto.jsx
@@ -5,10 +5,11 @@ export default class UserPhoto extends React.Component {
     const url = this.props.user.avatar_url;
     const username = this.props.user.login;
     const size = this.props.size;
+    const profileUrl = this.props.user.html_url;
 
     return (
       <div className="user-photo">
-        <img width={size} height={size} src={url} alt={username} title={username} />
+        <a target="_blank" href={profileUrl}><img width={size} height={size} src={url} alt={username} title={username} /></a>
       </div>
     );
   }
