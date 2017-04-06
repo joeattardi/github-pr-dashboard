5ad4529bd6dd3c0bbf7a7e332d48d17e839e46a8
Joe Attardi <jattardi@constantcontact.com>
2017-04-06 12:54:41 -0400

    Basic express server

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+var app = express();
+
+app.use(express.static('dist'));
+
+console.log('GitHub PR Dashboard');
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
